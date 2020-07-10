c9decdff24d80352b7b1e9ddfbcad72b21e0c1e5
grpargeter <pargetergregr@johndeere.com>
2020-07-10 14:35:59 -0500

    added start button functionality

--- sei/project/simon/js/script.js
+++ sei/project/simon/js/script.js
@@ -8,25 +8,48 @@ const yellowBtn = document.querySelector("#yellow");
 const startBtn = document.querySelector("#start");
 const title = document.querySelector("#title");
 const counter = document.querySelector("#counter");
-let sequence = [];
+// create variables and arrays for game play.
+let compSequence = [];
 let playerSequence = [];
 let flash;
 let turn;
-let congrats;
+let alive;
+// let congrats; add this after for if player exceeds 10 turns
 let intervalId;
 let sound = true;
 
-const flash =
-  // console.log(counter);
+// console.log(counter);
 
-  //add click events to buttons
-  blueBtn.addEventListener("click", test);
+//add click events to buttons
+blueBtn.addEventListener("click", test);
 redBtn.addEventListener("click", test);
 greenBtn.addEventListener("click", test);
 yellowBtn.addEventListener("click", test);
-startBtn.addEventListener("click", test);
 
-// test event listeners
 function test() {
   console.log("I was clicked");
 }
+
+//configure what happens when user hits start, clear out variables
+startBtn.addEventListener("click", (event) => {
+  start();
+});
+function start() {
+  win = false;
+  compSequence = [];
+  playerSequence = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  counter.innerHTML = 1;
+  alive = true;
+
+  for (var i = 0; i < 25; i++) {
+    compSequence.push(Math.floor(Math.random() * 4) + 1);
+  }
+  console.log(compSequence);
+}
+// // test event listeners
+// function test() {
+//   console.log("I was clicked");
+// }
