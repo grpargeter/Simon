5517d61b38a094df462b2ee9a6e3f2c71a8c9ccd
grpargeter <pargetergregr@johndeere.com>
2020-07-10 13:51:14 -0500

    added variables and event listeners

--- sei/project/simon/js/script.js
+++ sei/project/simon/js/script.js
@@ -1,1 +1,32 @@
-console.log("hello there");
+//console.log("hello there");
+//create variables for DOM manipulation for squares, start button and counter
+
+const blueBtn = document.querySelector("#blue");
+const redBtn = document.querySelector("#red");
+const greenBtn = document.querySelector("#green");
+const yellowBtn = document.querySelector("#yellow");
+const startBtn = document.querySelector("#start");
+const title = document.querySelector("#title");
+const counter = document.querySelector("#counter");
+let sequence = [];
+let playerSequence = [];
+let flash;
+let turn;
+let congrats;
+let intervalId;
+let sound = true;
+
+const flash =
+  // console.log(counter);
+
+  //add click events to buttons
+  blueBtn.addEventListener("click", test);
+redBtn.addEventListener("click", test);
+greenBtn.addEventListener("click", test);
+yellowBtn.addEventListener("click", test);
+startBtn.addEventListener("click", test);
+
+// test event listeners
+function test() {
+  console.log("I was clicked");
+}
